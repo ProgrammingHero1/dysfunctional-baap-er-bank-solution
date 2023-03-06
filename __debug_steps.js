57c7ec0d160e1e56ab41ab1cfa796d4481394445
Programming Hero <53802153+ProgrammingHero1@users.noreply.github.com>
2023-03-05 20:25:02 -0600

    Add files via upload

---
+++ __debug_steps.js
@@ -0,0 +1,20 @@
+/* 
+Debug Steps
+1. Go to the live website. And try to reproduce the error.
+2. Check the console of the live website to see any error/exception 
+3. Read exception thoroughly. in  detail
+4. If the error is new to you: Copy the text of the error and search online 
+5. if a line number is showing, click on that line to see the code
+6. Add breakpoint. re run the code (refresh the website). and see whether you are 
+getting the expected values
+    6.1: if you are not getting the expected values, check which values you are not
+    getting. if needed a break point in the previous line
+    6.2 If needed, step into (F11) to a function
+
+
+*/
+
+/* 
+innerText --> value
+
+*/
---
+++ js/utilities.js
@@ -0,0 +1,19 @@
+function getInputFieldValueById(inputFieldId) {
+  const inputField = document.getElementById(inputFieldId);
+  const inputFieldValueString = inputField.value;
+  const inputFieldValue = parseFloat(inputFieldValueString);
+  inputField.value = "";
+  return inputFieldValue;
+}
+
+function getTextElementValueById(elementId) {
+  const textElement = document.getElementById(elementId);
+  const textElementValueString = textElement.innerText;
+  const textElementValue = parseFloat(textElementValueString);
+  return textElementValue;
+}
+
+function setTextElementValueById(elementId, newValue) {
+  const textElement = document.getElementById(elementId);
+  textElement.innerText = newValue;
+}
